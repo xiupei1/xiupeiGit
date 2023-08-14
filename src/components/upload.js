aa70a4c2e08e150f4cc2771e0b7b7231a3f93ce0
xiupei <xiupei96@163.com>
2023-08-14 10:38:33 +0800

    ccccc

--- src/components/upload.js
+++ src/components/upload.js
@@ -49,6 +49,7 @@ function uploadFile(file) {
 // 选择文件并触发上传
 // 6666
 // 111
+// wwwww
 let fileInput = document.getElementById('file-input');
 fileInput.addEventListener('change', function(event) {
     let file = event.target.files[0];
