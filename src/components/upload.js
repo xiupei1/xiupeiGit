5c5b7b84dedf205e27f4b5de8c6277edc8bc742e
xiupei <xiupei96@163.com>
2023-08-14 10:33:24 +0800

    合并删除2和3

--- src/components/upload.js
+++ src/components/upload.js
@@ -52,6 +52,4 @@ fileInput.addEventListener('change', function(event) {
     let file = event.target.files[0];
     uploadFile(file);
     console.log(111)
-    console.log(222)
-    console.log(333)
 });
